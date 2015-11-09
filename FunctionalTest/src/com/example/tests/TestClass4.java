5146648f1ff855155e13039ba8ee2eb6717b8a81
Filip Todoric <filiptodoric@gmail.com>
2015-11-08 23:37:44 -0500

    Only need to test AI, then moving to Cucumber testing

---
+++ FunctionalTest/src/com/example/tests/TestClass4.java
@@ -0,0 +1,110 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TestClass4 {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://localhost:8080/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+  
+  public void sendAlert()	{
+	    String promptAnswer = "2";
+	    driver.findElement(By.id("start")).click();
+	    Alert alert = driver.switchTo().alert();
+	    alert.sendKeys(promptAnswer);
+	    alert.accept();
+}
+
+  @Test
+  public void testClass4() throws Exception {
+    driver.get(baseUrl + "/blackjack/");
+    sendAlert();
+    try {
+      String test1 = driver.findElement(By.id("dealer_hand")).getText();
+      if(test1.contains("HIDDEN"))	{
+    	  test1 = "HIDDEN";
+      }
+      assertEquals("HIDDEN", test1);
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    try {
+      String test2 = driver.findElement(By.id("player1_hand")).getText();
+      if(test2.contains("HIDDEN") == false)	{
+    	  test2 = "";
+      }
+      assertEquals("", test2);
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    try {
+      String test3 = driver.findElement(By.id("player2_hand")).getText();
+      if(test3.contains("HIDDEN"))	{
+    	  test3 = "HIDDEN";
+      }
+      assertEquals("HIDDEN", test3);
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
