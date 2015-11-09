b626147f662f24cd826be04f474d634541cd46ee
Filip Todoric <filiptodoric@gmail.com>
2015-11-09 00:03:09 -0500

    done selenium tests

---
+++ FunctionalTest/src/com/example/tests/SeleniumTestSuite.java
@@ -0,0 +1,13 @@
+package com.example.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestClass1.class, TestClass10.class, TestClass2.class,
+		TestClass3.class, TestClass4.class, TestClass5.class, TestClass6.class,
+		TestClass7.class, TestClass8.class, TestClass9.class })
+public class SeleniumTestSuite {
+
+}
